0575cf86fd71ad036f48c3359d7cc1697eafc26c
hpe-ii-kaito <kaito.ii@hpe.com>
2025-02-05 16:46:17 +0900

    Add template test file and dependencies

--- scaffolder-templates/spring-boot/skeleton/src/test/java/com/example/${{values.java_package_name}}/${{values.component_id}}Test.java
+++ scaffolder-templates/spring-boot/skeleton/src/test/java/com/example/${{values.java_package_name}}/${{values.component_id}}Test.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTest {
+public class ${{values.component_id}}Test {
 
 	@Test
 	public void contextLoads() {
